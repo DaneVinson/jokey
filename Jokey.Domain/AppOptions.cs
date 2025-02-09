d121cbeeaab94167630a3deac2accb3f1f896fdd
DaneVinson <dane.vinson@gmail.com>
2025-02-08 19:23:35 -0800

    Updated how render mode is displayed

--- Jokey.Domain/AppOptions.cs
+++ Jokey.Domain/AppOptions.cs
@@ -4,7 +4,6 @@ public sealed class AppOptions
 {
     public string JokeUri { get; set; } = string.Empty;
     public string Joke2Uri { get; set; } = string.Empty;
-    public string RenderTier { get; set; } = "?";
     public string UriDescription { get; set; } = string.Empty;
     public string Uri2Description { get; set; } = string.Empty;
 }
